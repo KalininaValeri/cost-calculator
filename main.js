f10eaceba505deb40996a12a6b0505cc78449443
KalininaValeri <kalinina.valeri@gmail.com>
2016-09-28 11:37:44 +0700

    the collection of data form elements

--- main.js
+++ main.js
@@ -6,18 +6,18 @@ function formSelectLocation(locations) {
         var optionText = str.charAt(0).toUpperCase() + str.substr(1).toLowerCase();
 
         $location.append(
-            $('<option value="' + element.value + '">' + optionText + '</option>')
+            $('<option value="' + element.value + '">' + optionText + '</option>') //точка с запятой
         );
     })
 }
 
 function formSelectWeight(max_weight) {
     var i = 0;
-    var $selectWeight = $("#selectWeight");
+    var $selectWeight = $('#selectWeight');
 
-    while (i < max_weight) {
+    while (i < max_weight) { // цикл for в себе содержит увеличение инкремента, и больше туту подходит (дополнительно, по желанию)
         $selectWeight.append(
-            $('<option value="' + (i + 0.1).toFixed(1) + '">' + i.toFixed(1) + '-' + (i + 0.1).toFixed(1) + '</option>')
+            $('<option value="' + (i + 0.1).toFixed(1) + '">' + i.toFixed(1) + '-' + (i + 0.1).toFixed(1) + '</option>') //точка с запятой
         );
 
         i = i + 0.1;
@@ -26,54 +26,53 @@ function formSelectWeight(max_weight) {
 
 $(function () {
     $.ajax({
-        url: "http://emspost.ru/api/rest/?method=ems.get.locations&type=russia&plain=true",
-        jsonp: "callback",
-        dataType: "jsonp",
+        url: 'http://emspost.ru/api/rest/?method=ems.get.locations&type=russia&plain=true',
+        jsonp: 'callback',
+        dataType: 'jsonp',
         data: {
-            format: "json"
+            format: 'json'
         },
-
+        // пустая строка, ненужна!
         success: function (response) {
             formSelectLocation(response.rsp.locations);
         }
     });
 
     $.ajax({
-        url: "http://emspost.ru/api/rest/?method=ems.get.max.weight",
-        jsonp: "callback",
-        dataType: "jsonp",
+        url: 'http://emspost.ru/api/rest/?method=ems.get.max.weight',
+        jsonp: 'callback',
+        dataType: 'jsonp',
         data: {
-            format: "json"
+            format: 'json'
         },
-
+// пустая строка, ненужна!
         success: function (response) {
-            dataWeight = response;
-
+// пустая строка, ненужна!
             formSelectWeight(response.rsp.max_weight);
         }
     });
 
-    $('form').submit(function (eventObject) {
-        eventObject.preventDefault();
-
-        var params = 'http://emspost.ru/api/rest?method=ems.calculate&from=' +
-            encodeURIComponent($("#selectSend").val()) +
-            '&to=' + encodeURIComponent($("#selectGet").val()) +
-            '&weight=' + encodeURIComponent($("#selectWeight").val());
+    $( "form" ).on( "submit", function( event ) {
+        event.preventDefault();
+        var params ='http://emspost.ru/api/rest?method=ems.calculate&' +  $( this ).serialize();
+        console.log(params);
 
         $.ajax({
             url: params,
-            jsonp: "callback",
-            dataType: "jsonp",
+            jsonp: 'callback',
+            // нет обработки начала запроса, повешать прелоадер например или еще что то (дополнительно, по желанию), снять когда запрос вернется
+            dataType: 'jsonp',
             data: {
-                format: "json"
+                format: 'json'
             },
-
+// пустая строка, ненужна!
             success: function (response) {
-                $('div').removeClass('not-active');
+                $('div').removeClass('not-active'); // не конкретно, дивов может стать больше
+                // стоит скрвватиь эти элеменьы если значения в форме были изменены? (дополнительно, по желанию)
                 $('#price').text(response.rsp.price);
                 $('#term').text(response.rsp.term.min + '-' + response.rsp.term.max);
             }
+            // не обрабатываешь ошибки ни в одном запросе, если что то случитьсяпользователь даже не узнает, не попробует еще раз и перестанет пользоваться сервисом (дополнительно, по желанию)
         });
     });
 });
